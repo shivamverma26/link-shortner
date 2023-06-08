2f6ba7087d9f64b9eb14a93823dcebe2acd9a72d
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-06-08 10:48:48 +0530

    Delete end directory

--- end/InputForm.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import URL, DataRequired
-
-class InputForm(FlaskForm):
-    url = StringField("URL", validators=[DataRequired(), URL()])
-    submit = SubmitField("Shorten")
